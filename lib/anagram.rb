4093ebde64a74278e48de2a1e04975e666821d27
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-22 14:38:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    sorted_word = @word.chars.sort.join
+    anagrams = [];
+    possible_anagrams.each do |word|
+      sorted_anagram = word.chars.sort.join
+      if sorted_anagram == sorted_word
+        anagrams << word
+      end
+    end
+    anagrams
+  end
+end
